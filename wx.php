ab3ad236399e84962c8e88b81e598e2215eab193
webjust <604854119@qq.com>
2017-06-02 15:56:30 +0800

    11111111111111111111111111111111

--- wx.php
+++ wx.php
@@ -57,6 +57,74 @@ class wxModel
 
             $tousername = $postObj->ToUserName;
             $fromusername = $postObj->FromUserName;
+            $msgtype = $postObj->MsgType;
+            $keyword = trim($postObj->Content);
+
+            // 图文  -》 返回图文列表    其他任何关键   默认
+            if ($msgtype == 'text')
+            {
+                // 判断关键字，根据关键字来自定义回复的消息
+                if ($keyword == "图文")
+                {
+                    // php + mysql    读取数据库 拿到文章列表的数据
+                    $arr = array(
+                        array(
+                            'title' => "套路太深！唯品会对清空微博作出解释 网友：这广告6到飞",
+                            'date' => "2017-6-2",
+                            'url' => "http://www.chinaz.com/news/quka/2017/0602/715449.shtml",
+                            'description' => '日前，唯品会清空了官方微博，成功的引起了众人的注意。',
+                            'picUrl' => "http://upload.chinaz.com/2017/0602/6363201407728157524057839.jpeg"
+                        ),
+                        array(
+                            'title' => "刘强东章泽天向中国人民大学捐赠3亿 设人大京东基金",
+                            'date' => "2017-6-2",
+                            'url' => "http://www.chinaz.com/news/2017/0602/715434.shtml",
+                            'description' => '京东集团创始人、董事局主席兼首席执行官及京东集团今天下午在中国人民大学宣布',
+                            'picUrl' => "http://upload.chinaz.com/2017/0602/6363201407728157524057839.jpeg"
+                        ),
+                        array(
+                            'title' => "高通发布 QC 4+ 快充技术，让努比亚 Z17 当了一次“业界领先”",
+                            'date' => "2017-6-2",
+                            'url' => "http://www.chinaz.com/mobile/2017/0602/715429.shtml",
+                            'description' => '充电 5 分钟，通话 2 小时这句广告词',
+                            'picUrl' => "http://upload.chinaz.com/2017/0602/6363201407728157524057839.jpeg"
+                        )
+                    );
+                    $textTpl = <<<EOT
+                                <xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <ArticleCount>%s</ArticleCount>
+                                <Articles>
+EOT;
+
+                    $str = "";
+                    foreach ($arr as $v)
+                    {
+                        $str .= "<item>";
+                        $str .= "<Title><![CDATA[".$v['title']."]]></Title>";
+                        $str .= "<Description><![CDATA[".$v['description']."]]></Description>";
+                        $str .= "<PicUrl><![CDATA[".$v['picUrl']."]]></PicUrl>";
+                        $str .= "<Url><![CDATA[".$v['url']."]]></Url>";
+                        $str .= "</item>";
+                    }
+
+                    $textTpl .= $str;
+                    $textTpl .= "</Articles></xml>";
+
+                    $time = time();
+                    $msgtype = 'news';
+                    $nums = count($arr);
+
+                    // Return a formatted string
+                    $retStr = sprintf($textTpl, $fromusername, $tousername, $time, $msgtype, $nums);
+                    echo $retStr;
+                }
+            }
+
+
             $time = time();
             $msgtype = $postObj->MsgType;
             $content = "欢迎来到微信公众号的开发世界！__GZPHP27";
